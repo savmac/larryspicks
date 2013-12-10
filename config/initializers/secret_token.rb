66ba911d1f57fe92fbd53f4ae956781693f5ebfc
Scott Knight <scotteknight@gmail.com>
2013-12-10 15:22:20 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Larryspicks::Application.config.secret_key_base = '9ccc29cd78fe8f7b487dc1615719fa32d4ed609124484f76f4b56fd64ef8791b79ba4cfa0cb2a3c6c400ea2fd050b2a81998603cb75aa610c3b7e20a0a7e3a72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Larryspicks::Application.config.session_store :cookie_store, key: '_larryspicks_session'
