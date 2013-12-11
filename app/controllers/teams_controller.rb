a18182da004d5b1565324348c1c3ae9e32a12478
Scott Knight <scotteknight@gmail.com>
2013-12-11 15:52:55 -0800

    Add edit and delete actions

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,17 +8,37 @@ class TeamsController < ApplicationController
 
 	def create
 		@team = sport_class.new(team_params)
-
 		if @team.save
+			flash[:success] = "Team added!"
 			redirect_to action: :index
 		end
+	end
 
+	def edit
+		@team = sport_class.find(params[:id])
+	end
+
+	def update
+		@team = sport_class.find(params[:id])
+		if @team.update(team_params)
+			flash[:success] = "Team updated!"
+			redirect_to action: :index
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@team = sport_class.find(params[:id])
+		@team.destroy
+		flash[:warning] = "Team deleted!"
+		redirect_to action: :index
 	end
 
 	private
 
 	def team_params
-		params.require(sport.underscore.to_sym).permit(:name)
+		params.require(sport.underscore.to_sym).permit(:city, :name, :abbreviation)
 	end
 
 	def set_sport
